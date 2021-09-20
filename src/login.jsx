5b8c98a07903da4c4306fe18f964cf1f07eeffc5
Tanmoy Sarkar <ts741127@gmail.com>
2021-09-20 20:05:40 +0530

    Sumnashu updates

--- src/login.jsx
+++ src/login.jsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import { ENDPOINT } from './config';
 import axios from "axios";
 import FormData from 'form-data';
+import './components/style.css'
+import './components/common.css'
 
-const LoginUser = (props, context)=>{
+const LoginUser = (props, context) => {
     const [step, setStep] = useState(1);
     // This is like enum
     // 1: OTP Request
@@ -13,34 +15,34 @@ const LoginUser = (props, context)=>{
     const [otp, setOtp] = useState('');
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState('');
- 
-    function requestOTP(){
+
+    function requestOTP() {
         setIsLoading(true);
         var data = new FormData();
         data.append('phoneno', phoneno);
         var config = {
             method: 'post',
             url: `${ENDPOINT}/api/otp/request/`,
-            data : data
-          };
-          
+            data: data
+        };
+
         axios(config)
-        .then(function (response) {
-            if(response.data.success){
-                setOtpId(response.data.otp_id);
-                setStep(2);
-                setError('');
-            }else{
-                setError(response.data.error);
-            }
-        })
-        .catch(function (error) {
-            setError("Failed to request");
-        });
+            .then(function (response) {
+                if (response.data.success) {
+                    setOtpId(response.data.otp_id);
+                    setStep(2);
+                    setError('');
+                } else {
+                    setError(response.data.error);
+                }
+            })
+            .catch(function (error) {
+                setError("Failed to request");
+            });
         setIsLoading(false);
     }
 
-    function verifyOTP(){
+    function verifyOTP() {
         setIsLoading(true);
         var data = new FormData();
         data.append('otp_id', otpId);
@@ -49,26 +51,26 @@ const LoginUser = (props, context)=>{
         var config = {
             method: 'post',
             url: `${ENDPOINT}/api/otp/verify/`,
-            data : data
+            data: data
         };
 
         axios(config)
-        .then(function (response) {
-            console.log(response);
-            if(response.data.success){
-                setError('');
-                loginUserPostOperation(otpId, response.data.otp_token);
-            }else{
-                setError(response.data.error);
-            }
-        })
-        .catch(function (error) {
-            setError("Failed to request");
-        });
+            .then(function (response) {
+                console.log(response);
+                if (response.data.success) {
+                    setError('');
+                    loginUserPostOperation(otpId, response.data.otp_token);
+                } else {
+                    setError(response.data.error);
+                }
+            })
+            .catch(function (error) {
+                setError("Failed to request");
+            });
         setIsLoading(false);
     }
 
-    function loginUserPostOperation(otp_id, otp_token){
+    function loginUserPostOperation(otp_id, otp_token) {
         var data = new FormData();
         data.append('otp_id', otp_id);
         data.append('otp_token', otp_token);
@@ -76,43 +78,78 @@ const LoginUser = (props, context)=>{
         var config = {
             method: 'post',
             url: `${ENDPOINT}/api/login/`,
-            data : data
+            data: data
         };
 
         axios(config)
-        .then(function (response) {
-            console.log(response);
-            if(response.data.success){
-                setError('');
-                localStorage.setItem('token', response.data.token);
-                window.location.href = '/';
-            }else{
-                setError(response.data.error);
-            }
-        })
-        .catch(function (error) {
-            setError("Failed to request");
-        });
+            .then(function (response) {
+                console.log(response);
+                if (response.data.success) {
+                    setError('');
+                    localStorage.setItem('token', response.data.token);
+                    window.location.href = '/';
+                } else {
+                    setError(response.data.error);
+                }
+            })
+            .catch(function (error) {
+                setError("Failed to request");
+            });
     }
 
     return (
         <>
-        {step === 1 ?(
-            <div>
-                <label>Enter Mobile no  </label>
-                <input value={phoneno} onChange={(event)=>{setPhoneno(event.target.value)}} /><br/>
-                <button onClick={()=>{requestOTP()}}>Request OTP</button>
-            </div>
+            {step === 1 ? (
+                <div>
+                    <h1 className="text-center header">Kampus Vire</h1>
+
+                    <div className="my-5 container">
+                        <label for="phoneNumber" className="form-label m-0 nunito_sans fs-4">Your Mobile Number</label><br />
+                        <small className="text-muted open_sans">We will send an otp for verification</small>
+                        <input type="phone" className="form-control mt-4 fst-italic" id="phoneNumber" placeholder="Phone Number" value={phoneno} onChange={(event) => { setPhoneno(event.target.value) }} />
+                        {/* <!--When OTP is send Display This in addtion--> */}
+                        {/* <p className ="fs-6 text-center my-3 rubik">
+                        Didn’t recieve OTP?<b className ="text-primary">Resend</b> in  <b>60s</b> <br/>
+                        <a href="#" className ="text-decoration-none rubik">Change phone Number</a>
+                        </p> */}
+                    </div>
+                    <div className="d-grid gap-2 d-md-block container button">
+                        <button className="btn bg-green text-light" type="button" onClick={() => { requestOTP() }}>Continue</button>
+                    </div>
+
+                    {/* <label>Enter Mobile no  </label>
+                    <input value={phoneno} onChange={(event) => { setPhoneno(event.target.value) }} /><br />
+                    <button onClick={() => { requestOTP() }}>Request OTP</button> */}
+                </div>
             )
-          : step === 2 ? (
-              <div>
-                <label>Enter Otp</label>
-                <input value={otp} onChange={(event)=>{setOtp(event.target.value)}} /><br/>
-                <button onClick={()=>{verifyOTP()}}>Verify OTP</button>
-              </div>
-              ) : 
-          (<div>Not yet</div>)
-          }
+                : step === 2 ? (
+
+                    <div>
+                    <h1 className="text-center header">Kampus Vire</h1>
+
+                    <div className="my-5 container">
+                        <label for="phoneNumber" className="form-label m-0 nunito_sans fs-4">Enter The OTP</label><br />
+                        <small className="text-muted open_sans">Please enter the OTP sent</small>
+                        <input type="phone" className="form-control mt-4 fst-italic" placeholder="Enter the OTP" value={otp} onChange={(event) => { setOtp(event.target.value) }} />
+                        {/* <!--When OTP is send Display This in addtion--> */}
+                        <p className ="fs-6 text-center my-3 rubik">
+                        Didn’t recieve OTP?<b className ="text-primary">Resend</b> in  <b>60s</b> <br/>
+                        <a href="#" className ="text-decoration-none rubik">Change phone Number</a>
+                        </p>
+                    </div>
+                    <div className="d-grid gap-2 d-md-block container button">
+                        <button className="btn bg-green text-light" type="button" onClick={() => { verifyOTP() }}>Continue</button>
+                    </div>
+
+
+
+                        {/* <label>Enter Otp</label>
+                        <input value={otp} onChange={(event) => { setOtp(event.target.value) }} /><br />
+                        <button onClick={() => { verifyOTP() }}>Verify OTP</button> */}
+                    </div>
+                ) :
+                    (<div>Not yet</div>)
+            }
         </>
     );
 }
--- src/products.jsx
+++ src/products.jsx
@@ -1,18 +1,23 @@
-import React , {useState} from 'react';
-import {useParams} from 'react-router-dom';
-import { ENDPOINT, GRAPHQL_ENDPOINT } from './config';
-import axios from 'axios';
-import { addToCart } from './shopping_function';
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { ENDPOINT, GRAPHQL_ENDPOINT } from "./config";
+import axios from "axios";
+import { addToCart } from "./shopping_function";
+import "./components/canteen.css";
+import canteenImage from "./components/img/Rectangle 100 (1).png";
+import { ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const AllProducts = ()=>{
-    const {shopid} = useParams();
-    const [products, setProducts] = useState([]);
-    const [loaded, setLoaded] = useState(false);
-    const token = localStorage.getItem('token');
+const AllProducts = (props) => {
+  const { shopid } = useParams();
+  const [products, setProducts] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const token = localStorage.getItem("token");
 
-    if(!loaded){
-        var data = JSON.stringify({
-            query: `query($shopId : Int!){
+  if (!loaded) {
+    var data = JSON.stringify({
+      query: `query($shopId : Int!){
             allProducts(shopId : $shopId){
               pageInfo{
                 startCursor
@@ -33,55 +38,124 @@ const AllProducts = ()=>{
               }
             }
           }`,
-            variables: {"shopId":shopid}
+      variables: { shopId: shopid },
+    });
+
+    var config = {
+      method: "post",
+      url: GRAPHQL_ENDPOINT,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then(function (response) {
+        setLoaded(true);
+        let tmpData = [];
+        response.data.data.allProducts.edges.forEach(function (edge) {
+          tmpData.push({
+            objId: edge.node.objId,
+            name: edge.node.name,
+            picture: edge.node.picture,
+            price: edge.node.price,
+            isAvailable: edge.node.isAvailable,
           });
-          
-          var config = {
-            method: 'post',
-            url: GRAPHQL_ENDPOINT,
-            headers: { 
-                'Content-Type': 'application/json',
-                'Authorization' : `Bearer ${token}`
-            },
-            data : data
-          };
+          console.log(tmpData);
+        });
+        setProducts([...products, ...tmpData]);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-          axios(config)
-          .then(function (response) {
-                setLoaded(true);
-                let tmpData = [];
-                response.data.data.allProducts.edges.forEach(function(edge){
-                    tmpData.push({
-                        objId: edge.node.objId,
-                        name: edge.node.name,
-                        picture: edge.node.picture,
-                        price: edge.node.price,
-                        isAvailable: edge.node.isAvailable
-                    });           
-                    console.log(tmpData);                          
-                });
-                setProducts([...products, ...tmpData]);
-        })      
-          .catch(function (error) {
-            console.log(error);
-          });
-    }
+  return (
+    <>
+      <div
+        className="container-fluid sticky-top p-0 m-0 mb-4 w-100"
+        style={{ height: "162px" }}
+      >
+        <ToastContainer
+          position="top-right"
+          autoClose={1500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <img src={canteenImage} className="img-fluid w-100 h-100" />
+        <div className="container-fluid p-0 m-0 topBg text-light nunito_sans fs-1 d-flex flex-column-reverse align-items-center">
+          <div className="position-absolute top-0 d-flex w-100 justify-content-between p-2 fs-4">
+            <a  onClick={()=>props.history.goBack()}><i className="fas fa-arrow-left"></i></a>
+            <h3 className="fw-bold nunito_sans text-uppercase text-center d-inline pt-2">
+              menu
+            </h3>
+            <i className="fas fa-map-marker-alt text-warning"></i>
+          </div>
+          <div
+            className="d-flex justify-content-between align-items-center w-50 mb-1"
+            style={{ fontSize: "1rem" }}
+          >
+            <small className="open_sans">Closes in 8pm</small>
+            <small className="open_sans fw-bold text-danger">Busy</small>
+          </div>
+          <div className="text-center w-75 lh-1">Canteen 1</div>
+        </div>
+      </div>
 
-
-    return (
-        <>
-        <h1>Hello This a AllProducts page of shop {shopid}</h1>
-        {products.map(product=>{
-                return <div key={product.objId}>
-                    <h2>{product.name}</h2>
-                    <h3>{product.price}</h3>
-                    <h3>{product.objId}</h3>
-                    <img src={ENDPOINT + "/media/"+product.picture}></img>
-                    <button onClick={()=>{addToCart(shopid, product.objId)}}>Add to cart</button>
+      <div className="container px-3 mt-3 d-flex flex-wrap justify-content-around">
+        {products.map((product) => {
+          return (
+            <div
+              className="card  m-1 p-0 bg-light"
+              style={{ width: "47%" }}
+              key={product.objId}
+            >
+              <img
+                src={ENDPOINT + "/media/" + product.picture}
+                className="card-img-top"
+              />
+              <div className="card-body p-1">
+                <h6 className="card-title text-center open_sans fw-bold m-2 text-capitalize">
+                  {product.name}
+                </h6>
+                {/* <small className="card-text text-muted text-center d-block">Preparation time : 25min</small><br /> */}
+                <small className="card-text text-center d-block">
+                  Cost: <b className="text-danger"> &#8377;{product.price}</b>
+                </small>
+                <br />
+                <div className="d-grid">
+                  <button
+                    className="btn bg-green fs-6 text-light mb-2 mx-2 fw-bold rubik rounded-pill d-block food"
+                    onClick={() => {
+                      addToCart(shopid, product.objId);
+                      toast.success("Added in cart", {
+                        position: "top-right",
+                        autoClose: 1500,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                      });
+                    }}
+                  >
+                    Order here
+                  </button>
                 </div>
-            })}
-        </>
-    );
-}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default AllProducts;
