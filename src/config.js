a7208c8c570bbb19cc93fde45d5b2377669750f7
Tanmoy Sarkar <ts741127@gmail.com>
2021-09-23 04:10:45 +0530

    update latest mvp

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 
-exports.ENDPOINT = "http://ec2-13-126-181-62.ap-south-1.compute.amazonaws.com:8080";
+exports.ENDPOINT = "http://devhunt.in:8080";
 exports.GRAPHQL_ENDPOINT = exports.ENDPOINT + "/graphql";
