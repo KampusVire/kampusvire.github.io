7f58cc246dbe02467d5193ca6e884819a9c04f59
Tanmoy Sarkar <ts741127@gmail.com>
2021-09-22 13:05:08 +0530

    Transaction pages done

---
+++ src/orderLog.jsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+import {GRAPHQL_ENDPOINT} from "./config";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import QrReader from "react-qr-scanner";
+import icon from "./components/img/icon.png"
+import Navbar from "./components/Navbar";
+import toast, { Toaster } from 'react-hot-toast';
+import "./components/transaction_card.css";
+
+const OrderLog = ()=>{
+    return (<>
+        <h1>jgfhgdgf</h1>
+    </>)
+}
+
+export default OrderLog;
--- src/transaction_log.jsx
+++ src/transaction_log.jsx
@@ -1,9 +1,6 @@
 import axios from "axios";
 import {GRAPHQL_ENDPOINT} from "./config";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import QrReader from "react-qr-scanner";
-import icon from "./components/img/icon.png"
 import Navbar from "./components/Navbar";
 import toast, { Toaster } from 'react-hot-toast';
 import "./components/transaction_card.css";
